727413732c621997ee6a6196be147675e0ebcc9b
Fozzy <vladkazanbaev56@gmail.com>
2024-01-18 20:57:24 +0500

    123

--- Organaizer/Form1.Designer.cs
+++ Organaizer/Form1.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnB = new System.Windows.Forms.Button();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.txtL = new System.Windows.Forms.TextBox();
+            this.checkShow = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnB.Location = new System.Drawing.Point(15, 136);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(150, 50);
+            this.btnB.TabIndex = 15;
+            this.btnB.Text = "Войти";
+            this.btnB.UseVisualStyleBackColor = false;
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(12, 97);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(328, 20);
+            this.txtP.TabIndex = 14;
+            // 
+            // txtL
+            // 
+            this.txtL.Location = new System.Drawing.Point(12, 40);
+            this.txtL.Name = "txtL";
+            this.txtL.Size = new System.Drawing.Size(328, 20);
+            this.txtL.TabIndex = 13;
+            // 
+            // checkShow
+            // 
+            this.checkShow.AutoSize = true;
+            this.checkShow.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.checkShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkShow.Location = new System.Drawing.Point(105, 73);
+            this.checkShow.Name = "checkShow";
+            this.checkShow.Size = new System.Drawing.Size(106, 21);
+            this.checkShow.TabIndex = 12;
+            this.checkShow.Text = "Посмотреть";
+            this.checkShow.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Логин";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnExit.Location = new System.Drawing.Point(192, 136);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 50);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(354, 211);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.txtL);
+            this.Controls.Add(this.checkShow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Form1";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.TextBox txtL;
+        private System.Windows.Forms.CheckBox checkShow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
