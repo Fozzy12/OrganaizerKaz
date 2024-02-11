3d900c3d90177fadb43fea528d848b90e6f48603
Fozzy <vladkazanbaev56@gmail.com>
2024-02-11 20:53:09 +0500

    Private 1

--- Organaizer/Form1.Designer.cs
+++ Organaizer/Form1.Designer.cs
@@ -29,12 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnS = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnS
+            // 
+            this.btnS.Location = new System.Drawing.Point(323, 415);
+            this.btnS.Name = "btnS";
+            this.btnS.Size = new System.Drawing.Size(75, 23);
+            this.btnS.TabIndex = 0;
+            this.btnS.Text = "button1";
+            this.btnS.UseVisualStyleBackColor = true;
+            this.btnS.Click += new System.EventHandler(this.btnS_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnS);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnS;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
